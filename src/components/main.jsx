62f76aa7ed30c4d39d0a9e534af44bd05b5b32cd
christinxxv <cwilson39@missuouriwestern.edu>
2022-07-04 10:35:54 -0500

    template project setup

---
+++ src/components/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../css/index.css';
+
+
+export default function Main() {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Main from './components/main.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+  <Main />
+  </>
+  
+);
+
+
